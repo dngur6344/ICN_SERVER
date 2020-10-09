be215981f5b8bfa4cb4dc6783698c1ff28a404bf
woohyuk <dngur6344@gmail.com>
2020-10-09 23:42:51 +0900

    Add Applying Time Zone (UTC+9)

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -3,8 +3,17 @@ package com.example.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PracticeApplication {
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재시간 : " + new Date());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class, args);
--- src/main/java/com/example/practice/calendar/calendarController.java
+++ src/main/java/com/example/practice/calendar/calendarController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
+
+import static java.util.Calendar.HOUR;
 
 @RestController
 @RequestMapping(value = "/schedule")
@@ -19,12 +23,20 @@ public class calendarController {
 
     @RequestMapping(value="/all")
     public List<calendarDTO> getAll(){
-        return calendar.getAllSchedules();
+        Calendar cal = Calendar.getInstance();
+        List<calendarDTO> listing = calendar.getAllSchedules();
+        for(int i=0;i<listing.size();i++){//현재 시간대 맞추기(한국 시간대=UTC+9)
+            cal.setTime(listing.get(i).getMoment());
+            cal.add(HOUR, 9);
+            listing.get(i).setMoment(cal.getTime());
+        }
+        return listing;
     }
     @RequestMapping(value="/insertEvent",method= RequestMethod.GET)
     public void insertEvent(@RequestParam(name="moment") String moment, @RequestParam(name="title") String title, @RequestParam(name="description") String description){
         calendarDTO calendarDTO = new calendarDTO();
         SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+        //transFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         Date date = null;
         try {
             date = transFormat.parse(moment);
