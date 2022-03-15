ab30a3208f582eabdc9ce1aa0311fd08eede2fb6
woohyuk <dngur6344@gmail.com>
2022-03-15 22:22:21 +0900

    Elasticsearch에 연결하기 위한 작업 - Score 관련 DTO 생성

---
+++ src/main/java/com/example/practice/score/ScoreDTO.java
@@ -0,0 +1,41 @@
+package com.example.practice.score;
+
+import java.util.Date;
+
+public class ScoreDTO {
+    private Date regtime;
+
+    private String name;
+
+    private Integer score;
+
+    public ScoreDTO(Date regtime, String name, Integer score) {
+        this.regtime = regtime;
+        this.name = name;
+        this.score = score;
+    }
+
+    public Date getRegtime() {
+        return regtime;
+    }
+
+    public void setRegtime(Date regtime) {
+        this.regtime = regtime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
